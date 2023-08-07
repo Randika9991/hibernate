28b6a9168030ab183666eaec69b27208b7c262bf
Randika9991 <kumarasirirandika0706@gmail.com>
2023-08-08 00:55:22 +0530

    hibernate

---
+++ src/main/java/Launcher.java
@@ -0,0 +1,28 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+public class Launcher extends Application {
+    public static void main(String[] args) {
+        launch();
+
+    }
+    @Override
+    public void start(Stage stage) throws Exception {
+     URL resource = Launcher.class.getResource("lk.ijse.orm.view/cashierdashboard.fxml");
+        Parent load = FXMLLoader.load(resource);
+
+        stage.setScene(new Scene(load));
+       // stage.getIcons().add(new Image("lk.ijse.orm.assets/icons8-chilli-100.png"));
+        stage.setTitle("SPICY FLAVOUR");
+        stage.centerOnScreen();
+        stage.show();
+    }
+
+
+}
